08ab5a58701767a0e54cb8d8d174ce28501a919a
dorega <56598993+dorega@users.noreply.github.com>
2020-01-10 12:29:44 +0100

    Add files via upload

---
+++ Tarefa 3/script.js
@@ -0,0 +1,24 @@
+
+/*tarefa 3*/
+
+
+
+var leones = 5.6548 ;
+var leopardos = 7.8547 ;
+var gatitos = 9.5465 ;
+
+var cuadrado = Math.pow(leones,2);
+alert(cuadrado);
+
+var raíz = Math.sqrt(gatitos);
+alert(raíz);
+
+var pi = Math.PI;
+var redondeo = Math.round(pi);
+alert(redondeo);
+
+var random = Math.random(1,3);
+alert(random);
+
+var menor = Math.min(leones,leopardos,gatitos);
+alert(menor);

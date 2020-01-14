d3a285a498a3f0db83c012b05fd1c2d9e24a0c05
dorega <56598993+dorega@users.noreply.github.com>
2020-01-14 18:12:25 +0100

    Add files via upload

---
+++ Tarefa 5/script.js
@@ -0,0 +1,27 @@
+/*
+	Tarefa 5
+	Nome do ficheiro: script.js
+		Usado: alert(), prompt()
+
+
+var nome;
+nome = prompt("Como te chamas?   ");
+alert("Guau... que nome máis interesante " + nome);
+alert("Ti es unha grande persoa " + nome);
+alert("Vales moito " + nome);
+alert("Non hai ninguén máis elegante ca " + nome);
+alert("e... " + nome + ", o mundo adórate!");
+*/
+
+var ducias;
+ducias = prompt("Cantas ducias de ovos leva unha tortilla?     ")
+var número;
+número = parseInt(ducias);
+alert(número);
+
+var ovos;
+ovos = (12*número);
+alert(ovos);
+
+
+/* parseInt sirve para que solo coja los numeros de la respuesta de prompt*/
